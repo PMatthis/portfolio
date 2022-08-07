fd1a3937959f655f58168a6a9f4dfa14d820a4e7
Matthis PINON <matthis.pinon@gmail.com>
2022-08-07 19:45:45 +0200

    summary + photo

--- apps/app-portfolio/src/app/app.tsx
+++ apps/app-portfolio/src/app/app.tsx
@@ -4,7 +4,10 @@ import Presentation from './presentation/presentation';
 
 export function App() {
   return (
-    <Presentation />
+    <Presentation
+      summary="Je suis actuellement apprenti chez Euro-Information Développement,
+       me spécialisant dans les systèmes d'information et les technologies web"
+    />
   );
 }
 
--- apps/app-portfolio/src/app/presentation/presentation.tsx
+++ apps/app-portfolio/src/app/presentation/presentation.tsx
@@ -1,14 +1,23 @@
 import styles from './presentation.module.scss';
 
 /* eslint-disable-next-line */
-export interface PresentationProps {}
+export interface PresentationProps {
+  summary: string,
+
+}
 
 export function Presentation(props: PresentationProps) {
 
   return (
-    <div className="flex align-items-center flex-column text-white ">
-      <h1>Matthis PINON</h1>
-
+    <div className="flex align-items-center flex-row justify-content-center">
+      <div className='flex flex-column align-items-end'>
+        <h2 className='w-full mb-0 text-center'>Bonjour, je m'appel</h2>
+        <h1 className='w-full text-right'>Matthis PINON</h1>
+        <img src='..\..\assets\pictures\gettyimages-vert-appaisement.jpg' alt='portrait de Matthis PINON'
+          className='border-round-3xl w-9'
+        />
+      </div>
+      <p className='text-center w-4'>{props.summary}</p>
     </div>
   );
 }
